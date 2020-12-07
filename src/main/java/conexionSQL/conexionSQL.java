77cabe78548b81aae36cf63b0f03f9303b79ef33
Jonathan Israel Vásquez Vivas <jon2013.p@hotmail.com>
2020-12-06 21:41:34 -0500

    MVC Productos MYSQL/Java

---
+++ src/main/java/conexionSQL/conexionSQL.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexionSQL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author jon20
+ */
+public class conexionSQL {
+    Connection  conectar=null;
+    
+    public Connection conexion(){
+    try {
+        Class.forName("com.mysql.jdbc.Driver");
+        conectar=DriverManager.getConnection("jdbc:mysql://localhost/supermercado?serverTimezone=UTC","root","");
+        
+        JOptionPane.showMessageDialog(null, "Conexion Exitosa");
+    } catch (Exception e){
+        JOptionPane.showMessageDialog(null, "Error de conexion"+e.getMessage());
+    }
+    return conectar;
+}
+}
